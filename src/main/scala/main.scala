92f64f9b3190e1da9dc7b82e98d0616f42985a81
Ja911Sla <Janerikslama@gmail.com>
2025-04-05 00:36:43 +0200

    lappy

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -2,7 +2,7 @@ import de.htwg._
 @main
 def main(): Unit =
 
-  println("It works!!!!! hopefullybbbbbbbbb")
+  println("It works!!!!! hopefullybbbbbbbbbaaaaaaaa")
   val myGame = Print();
   print(myGame.gameField())
 
